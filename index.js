f65b1deadb7276878df8431f1317422144ab32f3
Lung Chen <lungheichen@gmail.com>
2018-06-23 00:05:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+var katzDeli = [];
+
+
+function takeANumber (katzDeli, name) {
+  katzDeli.push(name)
+  return katzDeli.length + 1
+}
+
+function NowServing () {
+  if (katzDeli.length == 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return katzDeli[-1]
+  }
+}
+
+function currentLine (katzDeli) {
+  if (katzDeli.length > 0) {
+    var out = 'The line is currently: ';
+    for (var i = 0; i < katzDeli.length; i++) {
+      katzDeli[i]
+    }
+  } else {
+    return "The line is currently empty."
+  }
+
+}
